79980967e9aa10ef05ffd95d3489eda3ee00c520
irv205 <42255248+irv205@users.noreply.github.com>
2018-08-18 01:21:09 -0500

    Vista 2.0 responsive

--- resources/views/admin/entries/show.blade.php
+++ resources/views/admin/entries/show.blade.php
@@ -5,107 +5,92 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Document</title>
+    <title>orquideas</title>
+ 
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous"> -->
+    <link href="{{ asset('css/fa/css/all.css') }}" rel="stylesheet">
+
     <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script> -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/all.css') }}" rel="stylesheet" type="text/css">
+
 </head>
+
 <body>
-    <div class="container" id="app">
-        <h1>{{$entry->title}}</h1>
-        <p>{{$entry->synonym}}</p>
-        <p>{{$entry->distribution}}</p>
-        <p>{{$entry->description}}</p>
-        <p>{{ $url }}</p>
-        <hr>
-        <h1>jQuery UI Widget</h1>
-        <p>You can see this example with different themes on the <a href="http://plupload.com/example_jquery_ui.php">www.plupload.com</a> website.</p>
-
-        <form id="form" method="post" action="/echo/json">
-            <div id="uploader">
-                <p>Your browser doesn't have Flash, Silverlight or HTML5 support.</p>
-            </div>
-            <br />
-            <input type="submit" value="Submit" />
-        </form>
-    </div>
-    <script src="{{ asset('js/plupload.js') }}"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('js/jquery-ui.min.js') }}"></script>
-    <script src="{{ asset('js/jquery.ui.plupload.js') }}"></script>
-    <script type="text/javascript">
-        $(document).ready(function() {
-        $("#uploader").plupload({
-            // General settings
-            runtimes: 'html5,flash,silverlight,html4',
-            
-            // Fake server response here 
-            // url : '../upload.php',
-            url: "/echo/json",
-
-            // Maximum file size
-            max_file_size: '1000mb',
-
-            // User can upload no more then 20 files in one go (sets multiple_queues to false)
-            max_file_count: 20,
-            
-            chunk_size: '1mb',
-
-            // Resize images on clientside if we can
-            resize : {
-                width: 200, 
-                height: 200, 
-                quality: 90,
-                crop: true // crop to exact dimensions
-            },
-
-            // Specify what files to browse for
-            filters: [
-                { title: "Image files", extensions: "jpg,gif,png" },
-                { title: "Zip files", extensions:  "zip,avi" }
-            ],
-
-            // Rename files by clicking on their titles
-            rename: true,
-            
-            // Sort files
-            sortable: true,
-
-            // Enable ability to drag'n'drop files onto the widget (currently only HTML5 supports that)
-            dragdrop: true,
-
-            // Views to activate
-            views: {
-                list: false,
-                thumbs: true, // Show thumbs
-                active: 'thumbs'
-            },
-
-            // Flash settings
-            flash_swf_url : 'http://rawgithub.com/moxiecode/moxie/master/bin/flash/Moxie.cdn.swf',
-
-            // Silverlight settings
-            silverlight_xap_url : 'http://rawgithub.com/moxiecode/moxie/master/bin/silverlight/Moxie.cdn.xap'
-        });
-
-
-        // Handle the case when form was submitted before uploading has finished
-        $('#form').submit(function(e) {
-            // Files in queue upload them first
-            if ($('#uploader').plupload('getFiles').length > 0) {
-
-                // When all files are uploaded submit form
-                $('#uploader').on('complete', function() {
-                    $('#form')[0].submit();
-                });
-
-                $('#uploader').plupload('start');
-            } else {
-                alert("You must have at least one file in the queue.");
-            }
-            return false; // Keep the form from submitting
-        });
-    });
-    </script>
+  <div id="header">
+  <img align="left" src="{{ asset('storage/icon/orquideas.png') }}" height="60px">
+  <img align="right" src="{{ asset('storage/icon/orquideas.png') }}" height="60px"><br></br>
+  </div>
+  <hr>
 </body>
+
+<body>
+	<title>Orquidea1</title>
+
+	<div class="container" class="row fullscreen d-flex align-items-center justify-content-center" class="banner-content col-lg-4">
+					
+					
+		<h1>
+			<!-- Titulo -->	
+			<center> {{$entry->title}} <br> </center>
+		</h1>
+    <br>
+		<br>
+         <h4> <center> SINÓNIMO: </center> </h4>
+				<h3><center> {{$entry->synonym}} </center></h3>
+        <br>
+				<br>
+					<p class="pt-20 pb-20">
+
+					<!-- Informacion de la distribucion -->
+          <h4> <center>  DISTRIBUCIÓN: </center> </h4>
+
+						<div><center> {{$entry->distribution}} </center></div><br>
+
+						<!-- Fin de la informacion de la distribucion  -->
+
+					</p>
+
+						<div class="container">
+
+								<div>
+									<center>
+								<!-- imagen -->
+
+								<img src="{{ asset('storage/media/'.$entry->image.$entry->imageType) }}" width="509px" height="382">
+									
+								<!-- fin de la imagen -->
+									</center>
+								</div>
+
+							<div class="container">
+								<br>
+								<br>
+								<br>
+							<h4> <center>  DESCRIPCIÓN: </center> </h4>
+									
+								<p ALIGN="justify">	
+								<!-- Texto descriptivo -->
+
+								<center> {{$entry->description}} </center> 	
+
+								<!-- Fin de texto descriptivo -->
+								</p>
+                
+								<br>
+								<br>
+
+								<div style="text-align: center">
+										<p><strong>Realizado por: Ing. FernandoPoblano</strong></p>
+										<p><strong>Flores finas de Teya S.P.R de R.L</strong></p>
+										<p><strong>Hacienda Teya, Kanasin, Yucatan, Mexico</strong></p>
+										<p><strong>SEMARNAT-UMA-VIV-0163-YUC-09</strong></p>
+								</div>	
+
+						</div>
+</body>	
+
 </html>
