fdca53f099b7dbaca98d9feb430c2dd642fd6362
irv205 <42255248+irv205@users.noreply.github.com>
2018-09-13 00:21:56 -0500

    show
    final

---
+++ app/Http/Controllers/QrController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use QrCode;
+
+class QrController extends Controller
+{
+    public function generate()
+    {
+        $var='Prueba';
+        return QrCode::errorCorrection('H')->size(300)->generate($var);        
+    }
+
+    public function generateUrl($id) 
+    {
+        return QrCode::errorCorrection('H')->size(300)->generate('http://localhost/museo-orquideas-master/public/entries/'.$id);
+    }
+}
+
+?>
